23341c24ebc572ad7d47337ee81037e60567166a
Jirakit <hwak001@gmail.com>
2022-05-19 23:57:37 +0700

    add

--- public/js/header/world.js
+++ public/js/header/world.js
@@ -14,3 +14,31 @@ function liveSearch() {
       }
     }
 }
+
+function sort(event){
+var sortBy = document.querySelector('#sort-by');
+    item = document.querySelectorAll('.search');
+    ul = document.getElementById("myUL");
+    let itemsArray = Array.prototype.slice.call(item,0);
+    itemsArray = itemsArray.sort(function(a, b) {
+    let aVal = ('stock' == sortBy.value) ? a.dataset.stock : ('rating' == sortBy.value) ? a.dataset.rating : ('none' == sortBy.value) ? a.dataset.none : ('price-high' == sortBy.value) ? parseInt(a.dataset.price) : ('price-low' == sortBy.value) ? parseInt(a.dataset.price) : a.dataset.name.toLowerCase();
+    let bVal = ('stock' == sortBy.value) ? b.dataset.stock : ('rating' == sortBy.value) ? b.dataset.rating : ('none' == sortBy.value) ? b.dataset.none : ('price-high' == sortBy.value) ? parseInt(b.dataset.price) : ('price-low' == sortBy.value) ? parseInt(b.dataset.price) : b.dataset.name.toLowerCase();
+    if(sortBy.value == 'price-high' || sortBy.value == 'rating' || sortBy.value == 'stock'){
+        return aVal > bVal ? -1 : 1;
+    } else {
+        return aVal > bVal ? 1 : -1;
+    }
+    });
+    ul.innerHTML = '';
+    itemsArray.forEach((item,index) => {
+      ul.append(item);
+    });
+}
+
+var loadFile = function(event) {
+  var output = document.getElementById('output');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function() {
+    URL.revokeObjectURL(output.src) // free memory
+  }
+};
